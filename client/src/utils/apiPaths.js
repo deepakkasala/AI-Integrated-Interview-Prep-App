cba089806d68a8fb7a4006e9ce341711dc58a054
Deepak Kasala <deepakkasala14@gmail.com>
2025-10-17 18:24:43 +0530

    Add .env file for environment variables and update BASE_URL logic in apiPaths.js

--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,5 +1,7 @@
 const PORT = 3060;
-export const BASE_URL = `http://localhost:${PORT}`;
+// export const BASE_URL = `http://localhost:${PORT}`;
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || `http://localhost:${PORT}`;
 
 export const API_PATHS = {
   AUTH: {
