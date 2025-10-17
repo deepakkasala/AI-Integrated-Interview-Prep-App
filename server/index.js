84e23dcf8f56d1440d6d2738e1df2af68f9df77f
Deepak Kasala <deepakkasala14@gmail.com>
2025-10-17 19:53:16 +0530

    Enhance CORS configuration and implement memory storage for file uploads

--- server/index.js
+++ server/index.js
@@ -10,14 +10,29 @@ const questionRoutes = require("./routes/question");
 const aiRoutes = require("./routes/ai");
 // Middleware to parse JSON bodies
 const port = process.env.PORT || 3060;
-// {
-//     origin: `http://localhost:${port}`,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   }
-app.use(cors());
-app.use(express.json());
+
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-integrated-interview-prep-app-cl.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 app.use("/auth", authRoutes);
